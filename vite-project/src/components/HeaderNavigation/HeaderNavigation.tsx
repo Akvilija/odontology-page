96243763755a722a99fb9d9ec9a653151fa21708
Akvilija <akvilija.skorupskaite199503@gmail.com>
2025-02-04 17:58:07 +0200

    fixed header responsive and services

--- vite-project/src/components/HeaderNavigation/HeaderNavigation.tsx
+++ vite-project/src/components/HeaderNavigation/HeaderNavigation.tsx
@@ -1,36 +1,53 @@
-import { NavLink } from 'react-router'
+import { NavLink } from 'react-router-dom';
 import doctors from '../../assets/images/gydytojai.png';
 import clinicPhoto from '../../assets/images/juvera.jpg';
 import servicesPhoto from '../../assets/images/paslaugoss.png';
+import { useState } from 'react';
 
 type HeaderNavigationProps = {
     onSectionChange: (image: string) => void;
 }
 
 const HeaderNavigation = ({ onSectionChange }: HeaderNavigationProps): JSX.Element => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => setIsMenuOpen(prevState => !prevState);
+    const closeMenu = () => setIsMenuOpen(false);
+
     return (
-        <nav className="flex items-center justify-center mr-7">
-            <ul className="flex space-x-10">
-                <li>
-                    <NavLink to='/' className={({ isActive }) => isActive ? 'text-[var(--active-color)]' : 'text-black'}
-                        onClick={() => onSectionChange(clinicPhoto)}>
+        <nav>
+            <button onClick={toggleMenu} className="xl:hidden p-2 text-xl cursor-pointer">
+                ☰
+            </button>
+            <ul className={`flex flex-col xl:flex-row xl:space-x-10 absolute xl:static top-20 left-0 w-full xl:w-auto bg-white xl:bg-transparent shadow-xl xl:shadow-none ${isMenuOpen ? 'block' : 'hidden'} xl:flex`}>
+                <li className='hover:bg-[var(--hover-color)] p-2'>
+                    <NavLink to='/' className={({ isActive }) => isActive ? 'text-[var(--active-color)]' : 'text-black' }
+                        onClick={() =>{onSectionChange(clinicPhoto); closeMenu();}}>
                         Apie Mus
                     </NavLink>
                 </li>
-                <li>
+                <li className='hover:bg-[var(--hover-color)] p-2'>
                     <NavLink to='/doctors' className={({ isActive }) => isActive ? 'text-[var(--active-color)]' : 'text-black'}
-                        onClick={() => onSectionChange(doctors)}>
+                        onClick={() => {onSectionChange(doctors); closeMenu();}}>
                         Gydytojai
                     </NavLink>
                 </li>
-                <li>
+                <li className='hover:bg-[var(--hover-color)] p-2'>
                     <NavLink to='/services' className={({ isActive }) => isActive ? 'text-[var(--active-color)]' : 'text-black'}
-                        onClick={() => onSectionChange(servicesPhoto)}>
+                        onClick={() => {onSectionChange(servicesPhoto); closeMenu();}}>
                         Paslaugos
                     </NavLink>
                 </li>
-                <li>Kainos</li>
-                <li>Kontaktai</li>
+                <li className='hover:bg-[var(--hover-color)] p-2'>
+                    <NavLink to='/prices' className="text-black" onClick={closeMenu}>
+                        Kainos
+                    </NavLink>
+                </li>
+                <li className='hover:bg-[var(--hover-color)] p-2'>
+                    <NavLink to='/contact' className="text-black" onClick={closeMenu}>
+                        Kontaktai
+                    </NavLink>
+                </li>
             </ul>
         </nav>
     );
--- vite-project/src/components/ServicesList/ServicesList.tsx
+++ vite-project/src/components/ServicesList/ServicesList.tsx
@@ -9,7 +9,7 @@ type servicesDataProps = {
 const ServiceList = ({ services }: servicesDataProps): JSX.Element => {
     return (
         <Container>
-            <ul className="grid justify-items-center grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
+            <ul className="grid justify-items-center grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
                 {services.map((service) => (
                     <li key={service.id}>
                         <ServicesItem service={service} />
